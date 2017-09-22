fe8c821fb9895e33efc71ab9b0cbda7955563a9e
Jerryl-Lim <30333761+Jerryl-Lim@users.noreply.github.com>
2017-09-22 12:01:05 +1000

    keep infowindow open when hide toilet line231

--- CURRENT/javascript.js
+++ CURRENT/javascript.js
@@ -228,6 +228,7 @@ checkbox.addEventListener('change', function () {
            markers[i].setMap(null);
 	   destMarker();
        }
+       infowindow.open(map, markerz);
    } else {
        for(i=0; i<markers.length; i++){
            markers[i].setMap(map);
